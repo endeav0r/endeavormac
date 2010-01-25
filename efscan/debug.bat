e82ece35c3d3eefe675b7fbf8950e4bb196da38d
endeavormac <endeavormac@94cf5504-edb0-11de-bf31-4dec8810c1c1>
2010-01-25 02:55:38 +0000

    parsing of the NSRL hash files

--- efscan/debug.bat
+++ efscan/debug.bat
@@ -1,8 +1,11 @@
 gcc -O3 -g -pg -c -Wall src/polarssl/sha1.c -o o/md5.o
 gcc -O3 -g -pg -c -Wall src/polarssl/md5.c -o o/sha1.o
 
-gcc -O2 -g -pg -c -Wall src/fileinfo.c -o o/fileinfo.o
 gcc -O2 -g -pg -c -Wall src/utility.c -o o/utility.o
+gcc -O2 -g -pg -c -Wall src/nsrl.c -o o/nsrl.o
+gcc -O2 -g -pg -c -Wall src/fileinfo.c -o o/fileinfo.o
+gcc -O2 -g -pg -c -Wall src/error.c -o o/error.o
 
 gcc -O2 -g -pg -Wall src/test_fileinfo.c -o test_fileinfo.exe o/fileinfo.o o/utility.o o/sha1.o o/md5.o
+gcc -O2 -g -pg -Wall src/test_nsrlfile.c -o test_nsrlfile.exe o/utility.o o/nsrl.o o/error.o
 gcc -O2 -g -pg -Wall src/scan.c -o scan.exe o/fileinfo.o o/utility.o o/sha1.o o/md5.o o/sqlite3-o2.o
--- efscan/src/utility.h
+++ efscan/src/utility.h
@@ -15,8 +15,11 @@ char * int64_t_itoa (int64_t n);
 char * time_t_itoa (time_t t);
 char * int_itoa (int i);
 
+int64_t int64_t_atoi (char * s);
+
 // result must be freed
 char * base16_string (unsigned char * data, int data_len);
+int base16_char_to_int (unsigned char c);
 
 // result must be freed
 char * sqlite_escape_string (char * s);
