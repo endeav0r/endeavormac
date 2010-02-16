14cf20d6b8c3f2e3519f5927ce21953ba7934ec2
endeavormac <endeavormac@94cf5504-edb0-11de-bf31-4dec8810c1c1>
2010-02-16 19:08:02 +0000

    tanks rotate now when moving. it looks cool.

--- trunk/tanks2/src/tank.hpp
+++ trunk/tanks2/src/tank.hpp
@@ -11,27 +11,47 @@ extern "C" {
 
 #include "exception.hpp"
 
+#define TANK_ACTION_NONE 0
+#define TANK_ACTION_MOVE 1
+
+#define TANK_ORIENTATION_UP 0
+#define TANK_ORIENTATION_LEFT 192
+#define TANK_ORIENTATION_DOWN 128
+#define TANK_ORIENTATION_RIGHT 64
+
+typedef struct
+{
+	int type;
+	int x;
+	int y;
+} tank_action;
+
 class Game;
 
 class Tank
 {
 	private :
-		lua_State * l_vm;
 		int x;
 		int y;
+		int orientation;
+		lua_State * l_vm;
 		Game * game;
+		tank_action action;
 	
 	public :
 		Tank (Game * gamestate, char * lua_source);
 		~Tank();
 		
 		void turn ();
+		void process_action (int action_type);
 		
 		void set_x (int x);
 		void set_y (int y);
 		int get_x();
 		int get_y();
+		int get_orientation();
 		Tank * get_this(); // never thought i'd need this... get it
+		
 		
 		void register_lua_functions ();
 		void register_lua_function (char * lua_name, int (*f) (lua_State *));
