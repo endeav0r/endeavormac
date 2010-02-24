c939ea8efb6dc23176cacd3cea9ece27c1798595
endeavormac <endeavormac@94cf5504-edb0-11de-bf31-4dec8810c1c1>
2010-02-24 23:01:00 +0000

    changes...

--- trunk/tanks2/src/main.cpp
+++ trunk/tanks2/src/main.cpp
@@ -16,12 +16,13 @@ int main ()
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0);
 	set_palette(desktop_palette);
 	install_keyboard();
+	install_timer();
 	
 	try
 	{
 		Game game;
-		game.add_team("tanks/square.lua", 7);
-		game.add_team("tanks/go_to_flag.lua", 7);
+		game.add_team("tanks/kill.lua", 1);
+		game.add_team("tanks/go_to_flag.lua", 1);
 		game.entry();
 	}
 	catch (Exception e)
